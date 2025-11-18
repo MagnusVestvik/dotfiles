ccfc27d5366bb8f823cf8d124529a0608476db70
MagnusVestvik <magnusvestvik@proton.me>
2025-11-18 17:57:06 +0100

    FEATURE: Update nvim config

--- nvim/lua/dapconfig.lua
+++ nvim/lua/dapconfig.lua
@@ -1,23 +1,25 @@
-local dap, dapui = require("dap"), require("dapui")
 local vim = vim
+local dap, dapui = require("dap"), require("dapui")
+local dapview = require("dap-view")
 require("dapui").setup()
 
 dap.listeners.before.attach.dapui_config = function()
-    dapui.open()
+    dapview.open()
 end
 dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
+    dapview.open()
 end
 dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
+    dapview.close()
 end
 dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
+    dapview.close()
 end
 
 vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
 vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-vim.keymap.set("n", "<Leader>du", ":lua require('dapui').toggle()<CR>", {})
+--vim.keymap.set("n", "<Leader>du", ":lua require('dapui').toggle()<CR>", {})
+vim.keymap.set("n", "<leader>du", dapview.toggle, { desc = "toggle [d]ap-[u]i" })
 
 vim.fn.sign_define(
     "DapBreakpoint",
